ed730aeb2aaaa89e2a9f944221a1200d2de8aa98
sayantan01 <cavi4762@gmail.com>
2022-01-05 22:19:16 +0530

    removed buildfile from gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-#import django_on_heroku as django_heroku
+import django_heroku
 from pathlib import Path
 import os
 
