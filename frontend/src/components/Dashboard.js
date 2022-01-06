df7275c768c1cdb5a64d283edcfaa3c6583c19d6
sayantan01 <cavi4762@gmail.com>
2022-01-06 10:09:30 +0530

    updated dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,8 +1,6 @@
-import React, { useEffect } from "react";
-import { Row } from "react-bootstrap";
+import React from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
-import { Helmet } from "react-helmet";
 
 function Dashboard(props) {
   return (
